75dc98f9fc7befd5f38606b2c06f59d0449a8f12
Morgan Kpassi <morgan.kpassi@gmail.com>
2022-11-28 11:23:46 +0100

    Ajout des fichiers de base pour créer le template wordpress

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+// Prise en charge des images mise en avant 
+add_theme_support('post-thumbnails');
+
+// Affichage du titre automatique
+add_theme_support('title-tags');
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php get_header(); ?>
+</head>
+
+<body>
+
+    <h1>Bonjour</h1>
+    <?php get_footer(); ?>
+</body>
+
+</html>
